8d1fb9cf3c40c4fd7f6513ee3e9112b96e4f755e
Ohitgit <113122486+Ohitgit@users.noreply.github.com>
2025-02-03 10:18:32 -0800

    requiremnt.text added again

--- mkgroup/wsgi.py
+++ mkgroup/wsgi.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-# from vercel_wsgi import VercelWSGIApp
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mkgroup.settings')
 
 application = get_wsgi_application()
